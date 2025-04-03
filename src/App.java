ac9876de084c6c7c4e6c981277183881d9a612eb
Erick Yunga <eyungac3@est.ups.edu.ec>
2025-04-03 09:37:45 -0500

    final2

--- src/App.java
+++ src/App.java
@@ -3,16 +3,16 @@ import controllers.ShellSort;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        int [] a = {-10,0,4,7,2,1,9,6};
+        int [] a = {-10,0,4,7,2,1,9,6,5};
         // Instancia 
         ShellSort sS = new ShellSort();
+        System.out.println("Arreglo Original");
+        sS.printArray(a);
         System.out.println("Orden Asendente");
         sS.sort(a,true);
         sS.printArray(a);
         System.out.println("Orden Desendente");
         sS.sort(a, false);
-        sS.printArray(a);
-        System.out.println("Arreglo Original");
         sS.printArray(a);
 
 
--- src/controllers/ShellSort.java
+++ src/controllers/ShellSort.java
@@ -1,46 +1,55 @@
 package controllers;
 
 public class ShellSort {
-    public void sort(int[] array, boolean asendete){
+    public void sort(int[] array, boolean asendete) {
         int n = array.length;
-        for (int gap = n/2;gap>=1;gap=gap/2 ) {
-            //insercion 
+        
         if (asendete) {
-            int contCambios=0;
-            for (int i = gap;i<n;i++){
-                int temp = array[i];
-                int j = i;
-                // Aqui se desplaza 'tamp' a su posicion correcta 
-                while (j>=gap && array[j-gap]>temp) {
-                    contCambios++;
-                    array[j]=array[j-gap];
-                    j -= gap;
+            for (int gap = n / 2; gap >= 1; gap = gap / 2) {
+                // insercion
+                int contCambios = 0;
+                for (int i = gap; i < n; i++) {
+                    int temp = array[i];
+                    int j = i;
+                    // Aqui se desplaza 'tamp' a su posicion correcta
+                    while (j >= gap && array[j - gap] > temp) {
+                        contCambios++;
+                        array[j] = array[j - gap];
+                        j -= gap;
+                    }
+    
+                    array[j] = temp;
                 }
-                
-                array[j]=temp;
-            }            
-            System.out.println("Cambios: "+ contCambios); 
+                printArray(array);
+                System.out.println("Cambios: " + contCambios);
+    
+            }
         }else{
-            int contCambios=0;
-            for (int i = gap;i<n;i++){
-                int temp = array[i];
-                int j = i;
-                
-                while (j>=gap && array[j-gap]<temp) {
-                    contCambios++;
-                    array[j]=array[j-gap];
-                    j -= gap;
+            for (int gap = n / 2; gap >= 1; gap = gap / 2) {
+                // insercion
+                int contCambios = 0;
+                for (int i = gap; i < n; i++) {
+                    int temp = array[i];
+                    int j = i;
+                    // Aqui se desplaza 'tamp' a su posicion correcta
+                    while (j >= gap && array[j - gap] < temp) {
+                        contCambios++;
+                        array[j] = array[j - gap];
+                        j -= gap;
+                    }
+    
+                    array[j] = temp;
                 }
-                
-                array[j]=temp;
-            }            
-            System.out.println("Cambios: "+ contCambios);
-        }
+                printArray(array);
+                System.out.println("Cambios: " + contCambios);
+    
+            }
         }
     }
-    public void printArray(int[] array){
+
+    public void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " - ");
+            System.out.print(array[i] + " , ");
         }
         System.out.println("\n");
     }
